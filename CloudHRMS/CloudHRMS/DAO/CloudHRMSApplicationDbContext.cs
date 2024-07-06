12bb081ef7479084f546ee10679753aff51b92a0
mrkyaing <akog02@gmail.com>
2024-07-06 21:37:34 +0700

    Cloud HRMS 3

--- CloudHRMS/CloudHRMS/DAO/CloudHRMSApplicationDbContext.cs
+++ CloudHRMS/CloudHRMS/DAO/CloudHRMSApplicationDbContext.cs
@@ -4,7 +4,8 @@ namespace CloudHRMS.DAO
 {
     public class CloudHRMSApplicationDbContext:DbContext
     {
-        public CloudHRMSApplicationDbContext(DbContextOptions<CloudHRMSApplicationDbContext> options):base(options) {}//Constructor changing to parent
+        //Constructor changing to parent
+        public CloudHRMSApplicationDbContext(DbContextOptions<CloudHRMSApplicationDbContext> options):base(options) {}
         //Register the DBSet Entities / Domain object to interface the database tables 
         public DbSet<PositionEntity>   Positions { get; set; }
     }
